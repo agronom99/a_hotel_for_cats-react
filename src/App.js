39e8a5fa188ebf13ed7c78e81d86f68f79c3757e
Vitalii MV <agronom9913@gmail.com>
2023-07-10 22:08:55 +0300

    2023.07.10 22-08

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import UncontrolledExample from './components/Carousel';
+import UncontrolledExampleReviews from './components/CarouselReviews';
 import Header from './components/Header';
 import SectionHome from './components/Section__home';
 import SectionConditions from './components/Section_conditions';
-import SectionSlider from './components/Section_slider';
 import './styles/App.scss';
- 
 
 
 function App() {
   return (
+    
     <>
+   
     <Header />
     <SectionHome/>
     <SectionConditions/>
-    <SectionSlider/>
+    <UncontrolledExample/>
+    <UncontrolledExampleReviews/>
     </>
   );
 }
