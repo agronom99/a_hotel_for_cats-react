198286836e647a2e40d1c94f6aa2cb5d76e397db
Vitalii MV <agronom9913@gmail.com>
2023-07-26 22:25:50 +0300

    2023.07.26 22-25

--- src/pages/Numbers.jsx
+++ src/pages/Numbers.jsx
@@ -3,27 +3,33 @@ import CartNumbers from "../components/CartNumbers/index";
 import numbers from "../assets/numbers.json";
 import Sort from "../components/ButtonSelection";
 
-
 function Numbers() {
   return (
     <div className="cart-numbers">
       <div className="numbers-title">
-        <h1>Наші номера</h1>
-        <Sort />
+        <div>
+          <h1>Наші номера</h1>
+          <div className="filter">filtr</div>
+        </div>
       </div>
-      {numbers.map((obj) => (
-        <CartNumbers
-          key={obj.id}
-          title={obj.title}
-          img={obj.imageUrl}
-          sizes={obj.sizes}
-          area={obj.area}
-          equipment={obj.equipment}
-          price={obj.price}
-        />
-      ))}
+      <div>
+        <Sort/>
+        <div style={{display:"flex", flexWrap:"wrap"}}>
+          {numbers.map((obj) => (
+            <CartNumbers
+              key={obj.id}
+              title={obj.title}
+              img={obj.imageUrl}
+              sizes={obj.sizes}
+              area={obj.area}
+              equipment={obj.equipment}
+              price={obj.price}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Numbers;
